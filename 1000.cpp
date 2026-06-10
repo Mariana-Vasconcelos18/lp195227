25539659fc53a87a0f2a8afb90ad5e28072bbdaa
Mariana-Vasconcelos18 <m.vasconcelos18@unifesp.br>
2026-06-10 12:39:45 -0300

    Create 1000.cpp

---
+++ 1000.cpp
@@ -0,0 +1,17 @@
+ --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mariana Rafaela Dos Santos Vasconcelos
+Linguagem   : C++20
+Problema  https://judge.beecrowd.com/pt/problems/view/1169
+Data        :02/06/2026
+Objetivo    : escrever hello world
+Aprendizado : logica c++
+-------------------------------------------------------------------------- 
+
+#include <iostream>
+ 
+int main() {
+ std::cout << "Hello World!" << std::endl;
+ 
+    return 0;
+}
