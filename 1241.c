96fa64e96d190aa2b39d7c52d635714d030ff3f9
Mariana-Vasconcelos18 <m.vasconcelos18@unifesp.br>
2026-06-10 11:15:44 -0300

    Create 1241.c

---
+++ 1241.c
@@ -0,0 +1,35 @@
+ --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mariana Rafaela Dos Santos Vasconcelos
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1241
+Data        : 05/05/2026
+Objetivo    :  verificar se B corresponde aos últimos dígitos de A.
+Aprendizado : uso de string, uso de vetores
+-------------------------------------------------------------------------- 
+
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    int N;
+    char A[1001], B[1001];
+
+    scanf("%d\n", &N);
+
+    for(int i = 0; i < N; ++i){
+        scanf("%s %s\n", &A, &B);
+
+        if(strlen(B) > strlen(A))   printf("nao encaixa\n");
+        else{
+            strcpy(A, A + (strlen(A) - strlen(B)));
+            if(!strcmp(A, B))       printf("encaixa\n");
+            else                    printf("nao encaixa\n");
+        }
+    }
+
+    return 0;
+}
+
+
+
