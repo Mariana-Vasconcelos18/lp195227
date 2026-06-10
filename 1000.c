a0c8bfdf89f50b6a4f65862abf4e3419b9964595
Mariana-Vasconcelos18 <m.vasconcelos18@unifesp.br>
2026-06-10 10:58:54 -0300

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,20 @@
+ --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mariana Rafaela Dos Santos Vasconcelos
+Linguagem   : C
+Problema    :http://judge.beecrowd.com/pt/problems/view/1000
+Data        : 28/04/2026
+Objetivo    : escrever hello word
+Aprendizado : uso do printf
+-------------------------------------------------------------------------- 
+
+  #include <stdio.h>
+ 
+int main() {
+ 
+   printf ("Hello World!\n");
+ 
+    return 0;
+}
+
+
